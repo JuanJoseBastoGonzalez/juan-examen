99ecfbadafd507b247e6084385fc86ee07c00b6c
JuanJoseBastoGonzalez <151982381+JuanJoseBastoGonzalez@users.noreply.github.com>
2024-03-13 09:57:57 -0500

    creacion y maquetacion

---
+++ main.js
@@ -0,0 +1,36 @@
+// (()=>{
+//     body.innerHTML+=`
+//     <div class="container">
+//         <section>
+//             <img class="imagenP"src="./images/answer-1-1.png">
+//         </section>
+//         </div>
+//         <br>
+//             <div class="container">
+//                <p>
+//                 <samp class="titulo1">¿cuanto cuesta desallorrar mi <samp class="titulo1p2 titulo1">app </samp></samp>
+//                 </p>
+//                 <br>
+//                 <P>
+//                     <samp class="titulo1p2 titulo1">gbp <samp class="titulo1">?</samp></samp>
+                    
+//                 </p> 
+//             </div>
+    
+//             <section class="container">
+//                 calcula de forma rápida el costo para crear tu app, contestando estas sencillas preguntas   </section>
+//             <section class="container">
+//                 <button class="botonEm"><a class="link" href="./calidad.html">EMPEZAR</a></button>
+//             </section>
+    
+//         </div>
+    
+//     `;
+// })
+
+(()=>{
+    fetch("http://154.38.171.54:3001/priceA");
+    :PUSH
+
+    
+
